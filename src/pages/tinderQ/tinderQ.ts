8ff720b10965f943dbe6141392afb4df10bc92c2
Cyprien <cypconnet@wanadoo.fr>
2017-12-07 09:39:48 +0100

    test cards

--- src/pages/tinderQ/tinderQ.ts
+++ src/pages/tinderQ/tinderQ.ts
@@ -46,8 +46,6 @@ export class TinderQPage {
   stackConfig: StackConfig;
   recentCard: string = '';
 
-  firstVoteUp: boolean = false;
-
   currentNode;
   currentQId;
   questionsId: Array<number>;
